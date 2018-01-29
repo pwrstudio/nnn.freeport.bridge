21376f6a3a46ea30143927441bb590be618cb928
pwrstudio <pwrpntr@gmail.com>
2018-01-29 21:28:55 +0100

    get ALL posts

--- get_content.js
+++ get_content.js
@@ -5,19 +5,33 @@ module.exports = () => {
   return new Promise((resolve, reject) => {
     console.log('– Getting content from prismic'.yellow)
 
-    Prismic.api('https://nnnfreeport.prismic.io/api/v2')
-      .then(api => {
-        return api.query('', {pageSize: 1000})
-      })
-      .then(
-        response => {
-          console.log('✓ Received content:'.green, String(response.results.length).green.underline)
+    var data = []
 
-          resolve(response.results)
-        },
-        err => {
-          reject(err)
-        }
-      )
+    // Recursive function to get all pages of posts
+    fetchPosts = pg => {
+      Prismic.api('https://nnnfreeport.prismic.io/api/v2')
+        .then(api => {
+          return api.query('', {page: pg, pageSize: 100})
+        })
+        .then(
+          response => {
+            console.log('response.page', response.page)
+            console.log('response.total_pages', response.total_pages)
+            if (response.page < response.total_pages) {
+              data = data.concat(response.results)
+              fetchPosts(++pg)
+            } else {
+              data = data.concat(response.results)
+              console.log('✓ Received content:'.green, String(data.length).green.underline)
+              resolve(data)
+            }
+          },
+          err => {
+            reject(err)
+          }
+        )
+    }
+
+    fetchPosts(1)
   })
 }
